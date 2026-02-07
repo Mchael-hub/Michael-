e3205b62bb6cb554f12c74926525be37b597193e
Mchael-hub <bermiomichael@gmail.com>
2026-02-07 17:44:17 +0800

    Add files via upload

---
+++ michael 2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BermioMichaelJohnJoshua
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double number1 = 15.75;
+            double number2 = -20.0;
+            double baseNum = 2.0;
+            double exponent = 3.0;
+
+            Console.WriteLine("Absolute value of " + number2 + " is: " + Math.Abs(number2));
+            Console.WriteLine("Higher value between " + number1 + " and 10 is: " + Math.Max(number1, 10));
+            Console.WriteLine(baseNum + " to the power of " + exponent + " is: " + Math.Pow(baseNum, exponent));
+            Console.WriteLine("Square root of 144 is: " + Math.Sqrt(144));
+            Console.WriteLine("Ceiling value of " + number1 + " is: " + Math.Ceiling(number1));
+
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ michael.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ComputeAverageApp
+{
+    class ComputeAverageProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 5 grades separated by new line:");
+
+            double g1 = Convert.ToDouble(Console.ReadLine());
+            double g2 = Convert.ToDouble(Console.ReadLine());
+            double g3 = Convert.ToDouble(Console.ReadLine());
+            double g4 = Convert.ToDouble(Console.ReadLine());
+            double g5 = Convert.ToDouble(Console.ReadLine());
+
+            double average = (g1 + g2 + g3 + g4 + g5) / 5;
+
+            int roundedAverage = (int)Math.Round(average);
+
+            Console.WriteLine("The average is " + average + " and round off to " + roundedAverage);
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
